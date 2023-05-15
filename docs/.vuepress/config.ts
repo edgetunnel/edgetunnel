8c35c357312d284b03d9ced06e2d23a7ecbac37c
zizifn <515868058@qq.com>
2023-05-16 00:00:47 +0800

    add docs

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,8 @@
+import { defineUserConfig } from "vuepress";
+
+export default defineUserConfig({
+  lang: "en-US",
+  title: "Hello VuePress",
+  description: "Just playing around",
+  base: "/edgetunnel/",
+});
