850563235523c4bce3443054daa15e424c5fe2cc
zizifn <515868058@qq.com>
2023-05-16 01:57:00 +0800

    add more docs

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,8 +1,25 @@
-import { defineUserConfig } from "vuepress";
+import { defaultTheme, defineUserConfig } from "vuepress";
 
 export default defineUserConfig({
   lang: "en-US",
   title: "Hello VuePress",
   description: "Just playing around",
   // base: "/edgetunnel/",
+  locales: {
+    // The key is the path for the locale to be nested under.
+    // As a special case, the default locale can use '/' as its path.
+    "/": {
+      lang: "en-US",
+      title: "Edge Tunnel",
+      description: "Running V2ray inside edge/serverless functions",
+    },
+    "/zh/": {
+      lang: "zh-CN",
+      title: "Edge Tunnel",
+      description: "Running V2ray inside edge/serverless functions",
+    },
+  },
+  theme: defaultTheme({
+    lastUpdated: true,
+  }),
 });
