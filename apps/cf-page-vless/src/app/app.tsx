22e3f9b7ecc81b260dd359f20186646e00814518
etfans <124616173+etfans@users.noreply.github.com>
2023-05-21 11:54:11 +0800

    Revert "delete code....."
    This reverts commit ae969d020fccb89306aa93890124e3e91bbc949d.

---
+++ apps/cf-page-vless/src/app/app.tsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+export function App() {
+  return (
+    <>
+      <div />
+    </>
+  );
+}
+
+export default App;
---
+++ libs/vless-js/src/index.ts
@@ -0,0 +1,7 @@
+export {
+  delay,
+  makeReadableWebSocketStream,
+  safeCloseWebSocket as closeWebSocket,
+  processVlessHeader,
+  vlessJs,
+} from './lib/vless-js';
